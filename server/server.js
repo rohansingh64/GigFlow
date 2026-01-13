dc86ce1cfbac4d3aaf220a5e3b6de26868f21ddb
rohansingh64 <rohansinghrohansingh64@gmail.com>
2026-01-14 02:26:51 +0530

    mmmll

--- server/server.js
+++ server/server.js
@@ -12,20 +12,32 @@ const app = express();
 // Middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://gigflowplatformbyrohan.netlify.app/"   // your Netlify URL
-  ],
-  credentials: true
-}));
 
+const allowedOrigins = [
+	"http://localhost:5173",
+	"https://gigflowplatformbyrohan.netlify.app",
+];
 
-// Routes 
+app.use(
+	cors({
+		origin: function (origin, callback) {
+			// allow requests with no origin (like mobile apps, curl, Postman)
+			if (!origin) return callback(null, true);
+
+			if (allowedOrigins.includes(origin)) {
+				return callback(null, true);
+			} else {
+				return callback(new Error("Not allowed by CORS"));
+			}
+		},
+		credentials: true,
+	})
+);
+
+// Routes
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/gigs", require("./routes/gigRoutes"));
 app.use("/api/bids", require("./routes/bidRoutes"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("Server running on port " + PORT));
-
